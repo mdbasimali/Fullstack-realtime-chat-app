1be15e95727aaa743d727425749e9914d4090fce
mdbasimali <mdbasimali440@gmail.com>
2026-05-13 16:19:56 +0530

    override onPermissionRequest inside MainActivity to auto-grant WebRTC media resources to secure scheme

--- frontend/android/app/src/main/java/com/chatzone/app/MainActivity.java
+++ frontend/android/app/src/main/java/com/chatzone/app/MainActivity.java
@@ -3,9 +3,11 @@ package com.chatzone.app;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.webkit.PermissionRequest;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import com.getcapacitor.BridgeActivity;
+import com.getcapacitor.BridgeWebChromeClient;
 
 public class MainActivity extends BridgeActivity {
     @Override
@@ -29,5 +31,24 @@ public class MainActivity extends BridgeActivity {
         if (needsRequest) {
             ActivityCompat.requestPermissions(this, permissions, 100);
         }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        
+        // Override WebChromeClient to explicitly grant WebRTC media resources 
+        // to our secure localhost scheme inside the WebView.
+        this.bridge.getWebView().setWebChromeClient(new BridgeWebChromeClient(this.bridge) {
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        request.grant(request.getResources());
+                    }
+                });
+            }
+        });
     }
 }
