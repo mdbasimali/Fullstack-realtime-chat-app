3928483a740d25e8ed8250053f833f812d3bb805
mdbasimali <mdbasimali440@gmail.com>
2026-05-20 04:39:10 +0530

    import useState in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import { Routes, Route, Navigate, useLocation, useNavigate } from "react-router-dom";
 import { useAuthStore } from "./store/useAuthStore";
